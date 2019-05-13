5a6e153a4c873f97b44315085bce787e8b750340
alex chalyy <alexchalyy@gmail.com>
2019-05-12 20:10:47 -0400

    test

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,4 @@
-//  This javascript performs all logic for making orders and picking them up on Deli website. Heroku paractice.
+//  This javascript performs all logic for making orders and picking them up on Deli website. Heroku paractice. test.
 
 var $dishText;
 
